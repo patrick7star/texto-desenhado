17ee3ac6f87dfaebbeb5f76f79504e4ccebb456b
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2022-08-11 15:39:47 -0300

    matriz-texto aperfeiçoada...
    porém ainda não utilizada, a base de códigos para aplicar-la é enorme.

---
+++ src/matriz_texto.rs
@@ -0,0 +1,236 @@
+
+/*!
+ Fazendo um objeto que cuida do desenho, como
+ funções relacionadas a ele diretamente: 
+ concatenação, aparação de espaços em brancos
+ e etc.
+*/
+
+
+/* capacidade baseado na dimensão máxima
+ * do laptop, que foi primeiramente 
+ * codificado. Resolução da tela dele é
+ * 1366x768 pixels. */
+const MAX_LARGURA: usize = 42;
+const MAX_ALTURA: usize = 151;
+const FUNDO: char = ' ';
+
+// para codificação.
+type MultiArray = Vec<Vec<char>>;
+// para encurtar os parâmetros.
+pub type MT = MatrizTexto;
+
+
+#[derive(Clone)]
+pub struct MatrizTexto {
+   matriz: Vec<Vec<char>>,
+   altura: u16,
+   largura: u16,
+}
+
+impl MatrizTexto {
+   // método construtor:
+   pub fn cria(altura: u16, largura: u16) -> Self {
+      let mut matriz: MultiArray;
+      matriz = Vec::with_capacity(MAX_LARGURA);
+      
+      for _ in 0..altura {
+         let mut linha: Vec<char>;
+         linha = Vec::with_capacity(MAX_ALTURA);
+         for _ in 0..largura
+            { linha.push(FUNDO); }
+         matriz.push(linha);
+      }
+      return MatrizTexto { matriz, altura, largura }
+   }
+
+   // muda célula na matriz.
+   pub fn set(&mut self, y: u16, x: u16, ch: char) 
+      { self.matriz[y as usize][x as usize] = ch; }
+
+   // obtém célula na matriz.
+   pub fn get(&self, y: u16, x: u16) -> char 
+      { self.matriz[y as usize][x as usize] }
+
+   /* tupla com dimensão da matriz: altura e 
+    * largura respectivamente. */
+   pub fn dimensao(&self) -> (u16, u16) 
+      { (self.altura, self.largura) }
+}
+
+// outros métodos mais excentricos.
+impl MatrizTexto {
+   // apara colunas em brancos à direita.
+   pub fn trim_right(&mut self) {
+      let coluna_em_branco: bool = {
+         let mut valor: bool = true;
+         let x = (self.largura-1) as usize;
+
+         for y in 0..(self.altura as usize) {
+            if !self.matriz[y][x].is_whitespace()
+               { valor = false; }
+         }
+         valor
+      };
+      for line in self.matriz.iter_mut() 
+         { line.pop(); }
+   }
+
+   // apara colunas em brancos à esquerda.
+   pub fn trim_left(&mut self) {
+      let coluna_em_branco: bool = {
+         let mut valor: bool = true;
+         for y in 0..(self.altura as usize) {
+            if !self.matriz[y][0].is_whitespace()
+               { valor = false; break; }
+         }
+         valor
+      };
+      for line in self.matriz.iter_mut() 
+         { line.remove(0); }
+   }
+
+   // acrescenta 'm' linhas na matriz.
+   fn aumenta_altura(&mut self, h: u16) {
+      // registro aumento vertical da matriz.
+      self.altura += h;
+
+      // forma uma linha com devida largura.
+      let mut linha_em_branco: Vec<char>;
+      linha_em_branco = Vec::with_capacity(MAX_LARGURA);
+      for _ in 0..self.largura 
+         { linha_em_branco.push(' '); }
+      // adiciona linha-em-branco no topo.
+      for _ in 0..h
+         { self.matriz.insert(0, linha_em_branco.clone()); }
+   }
+   
+   // acrescenta 'n' colunas.
+   fn aumenta_largura(&mut self, l: u16) {
+      // registro aumento vertical da matriz.
+      self.largura += l;
+      for linha in self.matriz.iter_mut() {
+         for _ in 0..l 
+            { linha.push(FUNDO); }
+      }
+   }
+
+   // equalisa a menor matrix-texto com a maior.
+   fn equaliza_matrizes(mt1:&mut MT, mt2:&mut MT) {
+      // altura de ambos.
+      let (h1, _) = mt1.dimensao();
+      let (h2, _) = mt2.dimensao();
+
+      if h1 > h2 {
+         let diferenca:u16 = h1 - h2;
+         mt1.aumenta_altura(diferenca);
+      } else if h1 < h2 {
+         let diferenca = h2 - h1;
+         mt2.aumenta_altura(diferenca);
+      } 
+   }
+}
+
+/* operações das matriz nela própria, ou
+ * geradora, que significa que tal operação
+ * resulta numa matriz-texto nova. */
+impl MatrizTexto {
+   /* concatena matriz-texto passado com a própria
+    * instância. O argumento(que é uma matriz-texto)
+    * será "consumida" dentro do método. */
+   pub fn concatena(&mut self, mut matriz: MT) {
+      let (_, l) = matriz.dimensao();
+      MT::equaliza_matrizes(self, &mut matriz); 
+      self.aumenta_largura(l);
+      for y in 0..self.altura {
+         for x in 0..l { 
+            let value = matriz.get(y, x);
+            self.set(y, x+l, value);
+         }
+      }
+   }
+
+
+   /* obtem a referência do objeto e, imprime
+    * ele via saída padrão. */
+   pub fn imprime(&self) {
+      for row in self.matriz.iter() {
+         for cell in row 
+            { print!("{}", cell); }
+         print!("\n");
+      }
+   }
+
+}
+
+// apenas métodos estáticos do objeto.
+impl MatrizTexto {
+   /* transforma uma string -- é necesário que
+    * ela seja múltilinha -- numa matriz-texto.
+    */
+   pub fn to_matriz(string: &str) -> Self {
+      let largura: usize = {
+         string.lines()
+         .map(|linha| linha.len())
+         .max().unwrap()
+      };
+      // mede baseado na quantia de quebra-de-linhas.
+      let altura: u32 = {
+         string.chars()
+         .map(|ch| ((ch == '\n') as u32) * 1)
+         .sum()
+      };
+
+      // só faz em certas condições estabelecidas.
+      if altura == 0 || altura == 1 || largura == 0 
+         { panic!("dimensões inviáveis para tarefa!"); }
+
+      let mut matriz = MatrizTexto::cria(
+         altura as u16, 
+         largura as u16
+      );
+      let mut iterador = string.chars();
+
+      for y in 0..(altura as u16) {
+         for x in 0..(largura as u16) {
+            match iterador.next() {
+               Some(_char) => 
+                  { matriz.set(y, x, _char); }
+               None => ()
+            }
+         }
+      }
+
+      return matriz;
+   }
+
+   /* a mesma função que o método acima, porém com
+    * resultante da nova matrix-texto formada. É 
+    * um método estático. Os argumentos são referências,
+    * e a ordem de concatenação é da esquerda à 
+    * direita, mesma da codificação dos parâmetros. */
+   pub fn concatena_matrizes(mt1:&MT, mt2:&MT) -> MT {
+      // clona ambos.
+      let mut mt1 = mt1.clone();
+      let mut mt2 = mt2.clone();
+      MT::equaliza_matrizes(&mut mt1, &mut mt2);
+      // dimensão após redimensionamento de alguma.
+      let ((a, l1), (_, l2)) = (
+         mt1.dimensao(), 
+         mt2.dimensao()
+      );
+      // matriz-texto resultante.
+      let mut mtr: MatrizTexto;
+      mtr = MT::cria(a, l1 + l2);
+      for y in 0..a {
+         // primeiro parâmetro paran nova matriz-texto.
+         for x in 0..l1 
+            { mtr.set(y, x, mt1.get(y, x)); }
+         // agora, copiando o segundo.
+         for x in 0..l2
+            { mtr.set(y, x+l1, mt2.get(y, x)); }
+      }
+      return mtr;
+   }
+}
+
